57975ee34b0cddc25d992c3e575f0227eb20cf8e
Alasdair McLeay <alasdair.mcleay@me.com>
2017-12-08 13:18:40 +0000

    added logging

--- index.js
+++ index.js
@@ -23,37 +23,80 @@ var sourcePackage = jsonfile.readFileSync(sourcePath);
 var targetPackage = jsonfile.readFileSync(targetPath);
 
 
-function merge(source, target) {
-  var merged = Object.assign(
-    {},
-    target,
-    source
-  );
-  // sort
+function sort(merged) {
   return Object.keys(merged).sort().reduce((sorted, key) => {
     sorted[key] = merged[key];
     return sorted;
   }, {});
 }
 
+function merge(target, source) {
+  return Object.assign(
+    {},
+    target,
+    source
+  );
+}
+
+function logChanges(merged, target, pkgKey) {
+  var changes = Object.keys(merged).reduce((acc, key) => {
+    if(merged[key] != target[key]) {
+      acc[key] = merged[key];
+    }
+    return acc;
+  }, {});
+
+  var maxLength = Object.keys(changes).reduce((acc, key) => Math.max(key.length, acc), 0);
+
+  if(Object.keys(changes).length) {
+    console.log();
+    console.log(pkgKey);
+    Object.keys(changes).forEach(key => {
+      if(merged[key] && target[key] && (merged[key] !== target[key])) {
+        console.warn(`${key.padStart(maxLength)}: ${target[key]} => ${merged[key]}`)
+      } else if (merged[key] && !target[key]) {
+        console.warn(`${key.padStart(maxLength)}: => ${merged[key]}`)
+      }
+    });
+  }
+}
+
+function processDependencies(sourcePkg, targetPkg, key) {
+  var source = sourcePkg[key];
+  var target = targetPkg[key];
+
+  var merged = merge(target, source);
+  var sorted = sort(merged, source);
+
+  logChanges(sorted, target, key);
+
+  // sort
+  return sorted;
+}
+
+
+
 if(sourcePackage.dependencies) {
-  targetPackage.dependencies = merge(
-    sourcePackage.dependencies,
-    targetPackage.dependencies
+  targetPackage.dependencies = processDependencies(
+    sourcePackage,
+    targetPackage,
+    'dependencies'
   );
 };
 
 if(sourcePackage.devDependencies) {
-  targetPackage.devDependencies = merge(
-    sourcePackage.devDependencies,
-    targetPackage.devDependencies
+  targetPackage.devDependencies = processDependencies(
+    sourcePackage,
+    targetPackage,
+    'devDependencies'
   );
 };
 
 if(sourcePackage.peerDependencies) {
-  targetPackage.peerDependencies = merge(
-    sourcePackage.peerDependencies,
-    targetPackage.peerDependencies
+  targetPackage.peerDependencies = processDependencies(
+    sourcePackage,
+    targetPackage,
+    'peerDependencies'
   );
 };
 
